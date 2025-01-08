57ab7697e42b443389abbb29e4807e69be7dbfd9
Yuki-11 <yuki.kondo.ab@gmail.com>
2025-01-08 21:16:48 +0900

    update datasets.__init__.py

--- trackeval/datasets/__init__.py
+++ trackeval/datasets/__init__.py
@@ -15,4 +15,4 @@ from .youtube_vis import YouTubeVIS
 from .head_tracking_challenge import HeadTrackingChallenge
 from .rob_mots import RobMOTS
 from .person_path_22 import PersonPath22
-from .smot4sb_challenge import SMOT4SBDataset
+from .smot4sb_challenge import SMOT4SBChallenge
